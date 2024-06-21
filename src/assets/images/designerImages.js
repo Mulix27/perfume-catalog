32dec8b8558de355b09c940dc38d1b953cfdec80
Mulix27 <brunoalejandromay@gmail.com>
2024-06-21 15:04:17 -0600

    Agregar mas diseñadores

--- src/assets/images/designerImages.js
+++ src/assets/images/designerImages.js
@@ -12,6 +12,9 @@ const designerImages = {
     'giorgio armani': require('./designers/giorgio_armani.png').default,
     'ariana grande': require('./designers/ariana_grande.jpg').default,
     moschino: require('./designers/moschino.png').default,
+    valentino: require('./designers/valentino.png').default,
+    azzaro: require('./designers/azzaro.png').default,
+    halloween: require('./designers/halloween.jpg').default,
     // Añade más diseñadores aquí
   };
   
